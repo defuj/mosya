ee28892ae744e6eb560a694fce0ac67c05a4e2fe
defuj <defuj.official@gmail.com>
2022-08-10 10:16:42 +0700

    add config for Typography

---
+++ lib/utils/typography.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Typography {
+  static const caption2 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const caption1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 10,
+  );
+}
