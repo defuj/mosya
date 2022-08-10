cdc29879ac03677902266999bbb33d6a053b2747
defuj <defuj.official@gmail.com>
2022-08-10 10:24:26 +0700

    change typography config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mosya/pages/auth/forgot_password_page.dart';
 import 'package:mosya/pages/auth/login_page.dart';
 import 'package:mosya/pages/auth/on_time_password_page.dart';
@@ -22,6 +23,71 @@ class Main extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'OpenSans',
         primarySwatch: Colors.orange,
+        textTheme: TextTheme(
+          headline1: GoogleFonts.roboto(
+            fontSize: 92,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -1.5,
+          ),
+          headline2: GoogleFonts.roboto(
+            fontSize: 57,
+            fontWeight: FontWeight.w300,
+            letterSpacing: -0.5,
+          ),
+          headline3: GoogleFonts.roboto(
+            fontSize: 46,
+            fontWeight: FontWeight.w400,
+          ),
+          headline4: GoogleFonts.roboto(
+            fontSize: 32,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          headline5: GoogleFonts.roboto(
+            fontSize: 23,
+            fontWeight: FontWeight.w400,
+          ),
+          headline6: GoogleFonts.roboto(
+            fontSize: 19,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.15,
+          ),
+          subtitle1: GoogleFonts.roboto(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.15,
+          ),
+          subtitle2: GoogleFonts.roboto(
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.1,
+          ),
+          bodyText1: GoogleFonts.openSans(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+          ),
+          bodyText2: GoogleFonts.openSans(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          button: GoogleFonts.openSans(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 1.25,
+          ),
+          caption: GoogleFonts.openSans(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.4,
+          ),
+          overline: GoogleFonts.openSans(
+            fontSize: 10,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 1.5,
+          ),
+        ),
       ),
       home: const SplashScreenPage(),
       onGenerateRoute: (setting) {
